ca15d76c468238f18740fe5357a9500a2b92fd42
Patrick Navarrete <patricknavarrete1998@gmail.com>
2021-05-28 09:33:08 +0800

    initial commit

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'yoursecret',
-    database: 'yourdburlhere'
+    database: 'mongodb+srv://patdevil01:Patrick@cluster0.ua4ja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
