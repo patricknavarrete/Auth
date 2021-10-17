ec9d51793ad3f9de96ed8da2b8f0c6300905f5d6
patricknavarrete <39878429+patricknavarrete@users.noreply.github.com>
2021-10-18 02:59:22 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.get('/dashboard', (req, res) => {
 //@route POST /adduser
 router.post('/adduser', actions.addNew)
 
+router.post('/addSuggestion', actions.addNewSuggestion)
+
 //@desc Authenticate a user
 //@route POST /authenticate
 router.post('/authenticate', actions.authenticate)
